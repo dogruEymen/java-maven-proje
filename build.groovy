bcc581bdf32588982c8c38798a8877ff800a92d1
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-22 12:38:36 +0300

    .

--- build.groovy
+++ build.groovy
@@ -73,7 +73,7 @@ node {
 
                 
                 echo 'GHCR Giriş Yapılıyor...'
-                sh "echo ${GHCR_PASSWORD} | docker login ${registry} -u ${GHCR_USER} --password-stdin"
+                sh "echo ${GHCR_PASSWORD} | usr/bin/docker login ${registry} -u ${GHCR_USER} --password-stdin"
 
                 echo "imaj gönderiliyor..."
                 sh "docker push ${fullImageName}"
