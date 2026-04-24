4aed34d062dd4f1b8a07409cfcf46ab9f9730d78
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-24 13:52:51 +0300

    "."

--- maven-project/src/main/java/HelloMaven.java
+++ maven-project/src/main/java/HelloMaven.java
@@ -1,7 +1,7 @@
 public class HelloMaven {
     public static void main(String[] args) {
         // Konsola mesaj yazdıran basit bir komut
-        System.out.println("Merhaba! Maven projen başarıyla oluşturuldu.");
+        System.out.println("Hello! Maven projen başarıyla oluşturuldu.");
 
         // Basit bir matematiksel işlem ile test edelim
         int sayi1 = 10;
