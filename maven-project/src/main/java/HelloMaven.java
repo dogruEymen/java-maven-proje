4453b89470dcee852cd2b8c0e0024fa602261cc6
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-27 15:05:10 +0300

    .

--- maven-project/src/main/java/HelloMaven.java
+++ maven-project/src/main/java/HelloMaven.java
@@ -4,7 +4,7 @@ public class HelloMaven {
         System.out.println("Hello! Maven projen başarıyla oluşturuldu.");
 
         // Basit bir matematiksel işlem ile test edelim
-        int sayi1 = 212;
+        int sayi1 = 2122;
         int sayi2 = 22200;
         int toplam = sayi1 + sayi2;
 
