0d6d994606e4b048f2af72df817903208a6feef9
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-27 14:06:03 +0300

    .

--- maven-project/src/main/java/HelloMaven.java
+++ maven-project/src/main/java/HelloMaven.java
@@ -5,7 +5,7 @@ public class HelloMaven {
 
         // Basit bir matematiksel işlem ile test edelim
         int sayi1 = 2222;
-        int sayi2 = 5220;
+        int sayi2 = 52200;
         int toplam = sayi1 + sayi2;
 
         System.out.println("Toplam sonucu: " + toplam);
